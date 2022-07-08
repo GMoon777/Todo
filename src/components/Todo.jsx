36f936c37dbf8a4d10980ad6f3fad5e1e5c01443
Gabriel <gabrielmoon777@gmail.com>
2022-07-08 19:42:42 +0100

    Refactored Filter

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -15,62 +15,21 @@ const TodoList = () => {
 
 
     const setTaskListLengthText = function () {
-        let list = []
-        if (!isFiltered) {
-            list = tasks
-        }
-        else {
-            list = preFilteredList
-        }
         if (showTaskListItems) {
             // has to be specifically 1 so that the correct grammar is used.
-            if (list.length === 1) {
-                return(`(${list.length} Item)`)
+            if (tasks.length === 1) {
+                return (`(${tasks.length} Item)`)
             }
-            else if (!list.length) {
-                return(``)
+            else if (!tasks.length) {
+                return (``)
             }
             else {
-                return(`(${list.length} Items)`)
+                return (`(${tasks.length} Items)`)
             }
         }
         else {
-            return(``)
+            return (``)
         }
-    }
-
-    
-
-    const filterTodoList = function (title, e) {
-        e.preventDefault(e);
-        if (userTextInputValue.length) {
-            if (todoListName === "Please enter a name for your item") {
-                setShowTaskListItems(false)
-                setTodoListName(todoListNameHolder)
-            }
-        }
-        let filteredList = []
-        tasks.map(item => {
-
-            if (!isFiltered) {
-                if (userTextInputValue.length) {
-                    if (item.title === title) {
-                        setPreFilteredList([...tasks])
-                        filteredList.push(item)
-                        setTasks([...filteredList])
-                        setIsFiltered(true)
-                    }
-                }
-            }
-            else {
-                setIsFiltered(false)
-                setTasks([...preFilteredList])
-                setUserTextInputValue("")
-            }
-            return true
-        }
-        )
-
     }
 
     const changeTodoListName = function (name, e) {
@@ -111,67 +70,28 @@ const TodoList = () => {
             }
         }
     }
-    const deleteTask = function (id) {
-        let list = []
-        if (!isFiltered) {
-            list = tasks
-        }
-        else {
-            list = preFilteredList
-        }
 
-        list.map(item => {
+    const deleteTask = function (id) {
+        tasks.map(item => {
             if (item.id === id) {
-                let check = list.indexOf(item)
-                let taskHolder = list
-                taskHolder.splice(check, 1,)
-                setTasks([...taskHolder])
+                let check = tasks.indexOf(item)
+                tasks.splice(check, 1)
+                setTasks(currentState => {
+                    return [...currentState]
+                })
                 setShowTaskListItems(true)
                 if (todoListName === "Please enter a name for your item") {
                     setTodoListName(todoListNameHolder)
                 }
             }
-            return true
         })
         if (isFiltered) {
-            setIsFiltered(false)
             setUserTextInputValue("")
         }
     }
-    // select a data source to work on based on if you are viewing a filter source or not
-    // not viewing filtered data so use main task list
-    // viewing filtered task list (only a couple of selected tasks) and so use the saved list (before filtering)
-    // accepts two arguments 1. the id of the task 2. a Boolean letting the function know if this action is to set the task as priority
-    // check to see if the pass in id matches one of the task objects mapped 
-    // check if this is a priority switch call 
-    // create a temporary task object
-    // check if the item has been completed before
-    // check to see if the task is marked as priority
-    // if it's not a priority task simply mark it as completed and timestamp the completion
-    // if it's a priority task mark it as completed and remove the priority flag
-    // if task has been completed before mark it as not completed 
-    // find the index of the task
-    // create a copy of the previous list of tasks
-    // replace the existing task with the updated task
-    // replace the task list in state with the updated task list
-    // function called with a priorityCall
-    // check to see if the task is already completed and if not...
-    // change the priority status of the task
-    // find the index of the task
-    // create a copy of the previous list of tasks
-    // replace the existing task with the updated task
-    // replace the task list in state with the updated task list              
-    // check if we are currently viewing a filtered list and toggle it off if so
 
     const updateCheckedState = function (id, priorityCall) {
-        let list = []
-        if (!isFiltered) {
-            list = tasks
-        }
-        else {
-            list = preFilteredList
-        }
-        list.map(item => {
+        tasks.map(item => {
             if (item.id === id) {
                 if (!priorityCall) {
                     let updatedTask = {}
@@ -186,26 +106,22 @@ const TodoList = () => {
                     else if (item.completedAt !== null) {
                         updatedTask = { ...item, completedAt: null, completed: !item.completed };
                     }
-                    let check = list.indexOf(item)
-                    let taskHolder = list
-                    taskHolder.splice(check, 1, updatedTask)
-                    setTasks([...taskHolder])
-                    if (isFiltered) {
-                        setIsFiltered(false)
-                        setUserTextInputValue("")
-                    }
+                    let check = tasks.indexOf(item)
+                    let taskHolder = tasks
+                    tasks.splice(check, 1)
+                    setTasks(currentState => {
+                        return [...currentState, updatedTask]
+                    })
                 }
                 else {
                     if (!item.completed) {
                         let updatedTask = { ...item, isPriority: !item.isPriority };
-                        let check = list.indexOf(item)
-                        let taskHolder = list
-                        taskHolder.splice(check, 1, updatedTask)
-                        setTasks([...taskHolder])
-                        if (isFiltered) {
-                            setIsFiltered(false)
-                            setUserTextInputValue("")
-                        }
+                        let check = tasks.indexOf(item)
+                        let taskHolder = tasks
+                        tasks.splice(check, 1)
+                        setTasks(currentState => {
+                            return [...currentState, updatedTask]
+                        })
                     }
                 }
             }
@@ -214,7 +130,6 @@ const TodoList = () => {
         )
     }
 
-
     return (
         <div className="app">
             <h1>{todoListName} {setTaskListLengthText()}</h1>
@@ -222,30 +137,59 @@ const TodoList = () => {
             <form>
                 <input placeholder="name..." value={userTextInputValue} onChange={(e) => setUserTextInputValue(e.target.value)}>
                 </input>
-                <button onClick={(e) => addTask(e)}>Add Task</button>
-                <button onClick={(e) => filterTodoList(userTextInputValue, e)}>{isFiltered ? 'Show All' : 'Filter By Name'}</button>
+                {!isFiltered ? <button onClick={(e) => addTask(e)}>Add Task</button> : null}
+
+                <button onClick={(e) => e.preventDefault(e) || setIsFiltered(!isFiltered)}>{isFiltered ? 'Show All' : 'Filter By Name'}</button>
                 <button onClick={(e) => e.preventDefault(e) || setSortBy("ABC")}>Sort By Alphabetical</button>
                 <button onClick={(e) => e.preventDefault(e) || setSortBy("Time")}>Sort By Time Created</button>
                 <button onClick={(e) => changeTodoListName(userTextInputValue, e)}>Change Todo List Name</button>
             </form>
             <ul>
                 {tasks
-                .sort((((a, b) => sortBy === "ABC" ? a.title.localeCompare(b.title) : a.createdAt > b.createdAt)))
-                .map(item => {
-                    return (
+                    .sort((((a, b) => sortBy === "ABC" ? a.title.localeCompare(b.title) : a.createdAt > b.createdAt)))
+                    .filter((item) => {
+                        if (isFiltered) {
+                            if (userTextInputValue.length) {
+                                if (item.title === userTextInputValue) {
+                                    if (todoListName === "Please enter the name of the task to filter") {
+                                        setTodoListName(todoListNameHolder)
+                                        setShowTaskListItems(true)
+                                    }
+                                    return true;
+                                }
 
-                        <div
-                            key={item.id}>
-                            <div className="tasksText" style={{ color: item.isPriority ? 'gold' : 'white', textDecoration: item.completed ? "line-through" : "", }}> {item.title + " "}
-                                <input type="checkbox" onChange={(e) => updateCheckedState(item.id, false)} checked={item.completed} />
-                                <button onClick={() => deleteTask(item.id)}>Delete</button>
-                                <button onClick={() => updateCheckedState(item.id, true)}>{item.isPriority ? 'Remove Priority' : 'Set as Priority'}</button>
+                            }
+                            else {
+                                if (todoListName !== "Please enter the name of the task to filter") {
+                                    setTodoListNameHolder(todoListName)
+                                    setTodoListName("Please enter the name of the task to filter")
+                                    setShowTaskListItems(false)
+                                }
+                            }
+                        }
+                        else {
+                            if (todoListName === "Please enter the name of the task to filter") {
+                                setTodoListName(todoListNameHolder)
+                                setShowTaskListItems(true)
+                            }
+                            return true
+                        }
+                    })
+                    .map(item => {
+                        return (
+
+                            <div
+                                key={item.id}>
+                                <div className="tasksText" style={{ color: item.isPriority ? 'gold' : 'white', textDecoration: item.completed ? "line-through" : "", }}> {item.title + " "}
+                                    <input type="checkbox" onChange={(e) => updateCheckedState(item.id, false)} checked={item.completed} />
+                                    <button onClick={() => deleteTask(item.id)}>Delete</button>
+                                    <button onClick={() => updateCheckedState(item.id, true)}>{item.isPriority ? 'Remove Priority' : 'Set as Priority'}</button>
+                                </div>
                             </div>
-                        </div>
 
-                    )
+                        )
 
-                })}
+                    })}
             </ul>
         </div>
 
